851cdf65ad629ebdb157573bf9c4444a55372d02
nukdokplex <nukdokplex@nukdokplex.ru>
2025-10-03 22:07:45 +0500

    fix(pkgs): fix hash mismatch in mbedtls-eddsa-src

--- pkgs/mbedtls-eddsa-src.nix
+++ pkgs/mbedtls-eddsa-src.nix
@@ -7,5 +7,5 @@ fetchFromGitHub {
   repo = "mbedtls";
   fetchSubmodules = true;
   rev = "6320af56726247352af5a003ae77f465f5b4f1c7";
-  hash = "sha256-wntpAcUE6EQlzxqM8jQCHMxBympGB/RTwFecYd+YPJk=";
+  hash = "sha256-4eiqm1+0bT4pEbmuvNJABRcCYHWYw8yLQwW0On7RU7c=";
 }
