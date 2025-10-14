c027072ac811d80d46125cb34528bf7d60656014
TheMaxMur <muravjev.mak@yandex.ru>
2025-10-15 02:39:33 +0300

    chore: add systems.flakeExposed

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     {
       overlays = import ./overlays.nix { inherit (inputs) outputs; };
     }
-    // flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (
+    // flake-utils.lib.eachSystem nixpkgs.lib.systems.flakeExposed (
       system:
       let
         pkgs = import nixpkgs {
